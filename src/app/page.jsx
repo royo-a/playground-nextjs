792a92016fa1d30936a47e66b91e48de3f8f77bb
Arnab Roy <royo.arnab@gmail.com>
2024-12-16 03:15:00 -0500

    Upload multiple images as blob and preview it.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,44 +1,64 @@
 'use client';
 
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import Image from 'next/image';
 
 export default function Home() {
-  const [image, setImage] = useState(null);
+  const [image, setImage] = useState([]);
 
-  const handleUploadImage = (e) => {
+  const handleUploadImage = useCallback((e) => {
+    console.group('handleUploadImage');
+    // 👉 Files is an array that contains all selected files
     const files = e.target.files;
     if (files.length > 0) {
-      const image = files[0];
-      console.log(image);
+      Array.from(files).forEach((file, index) => {
+        const fileReader = new FileReader();
+        fileReader.addEventListener(
+          'load',
+          (e) => {
+            if (index === 0) {
+              setImage(() => [e.target.result]);
+              return;
+            }
 
-      const fileReader = new FileReader();
-      fileReader.readAsDataURL(image);
-      fileReader.addEventListener('loadend', (e) => {
-        console.log(e.target.result);
-        setImage(e.target.result);
+            setImage((prevState) => [...prevState, e.target.result]);
+          },
+          { once: true }
+        );
+        fileReader.readAsDataURL(file);
       });
     }
-  };
+    console.groupEnd();
+  }, []);
+
+  const previewImages = useCallback(() => {
+    return image.map((img, index) => {
+      return (
+        <Image
+          key={imgx}
+          src={img}
+          height={200}
+          width={200}
+          alt="An image"
+          className="max-h-[200px] max-w-[200px] flex overflow-x-auto gap-2"
+        />
+      );
+    });
+  }, [image]);
 
   return (
     <div>
       <h1 className="text-3xl font-bold">
         We will upload an image and preview it
       </h1>
-      <div>
-        {image ? (
-          <Image
-            src={image}
-            height={400}
-            width={400}
-            alt="An image"
-            className="max-h-[200px] max-w-[200px]"
-          />
-        ) : null}
-      </div>
+      <div className="flex gap-2 ">{previewImages()}</div>
       <form>
-        <input type="file" accept="image/*" onChange={handleUploadImage} />
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleUploadImage}
+          multiple={true}
+        />
       </form>
     </div>
   );
